922d0945f9062d39e0075300986ca7161457cfc4
Blicharz <clblich@ilstu.edu>
2023-07-11 16:20:52 -0500

    Chapter 2

--- ch02.py
+++ ch02.py
@@ -1,1 +1,14 @@
 #Chapter 2
+import matplotlib.pyplot as plt
+import mglearn.datasets
+
+#generating a data set
+X, y = mglearn.datasets.make_forge()
+
+#plot dataset
+mglearn.discrete_scatter(X[:,0], X[:, 1], y)
+plt.legend(["Class 0", "Class 1"], loc = 4)
+plt.xlabel("First feature")
+plt.ylabel("Second feature")
+print("X.shape:", X.shape)
+plt.show()
