a02139868881a752699d75692a805a271c82bce0
Blicharz <clblich@ilstu.edu>
2023-08-03 14:01:26 -0500

    Linear regression

---
+++ ch02/Linear_Modeling/Boston_Housing_02.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import mglearn
+from IPython.display import display
+from sklearn.linear_model import LinearRegression 
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+data_url = "http://lib.stat.cmu.edu/datasets/boston"
+raw_df = pd.read_csv(data_url, sep="\s+", skiprows=22, header=None)
+data = np.hstack([raw_df.values[::2, :], raw_df.values[1::2, :2]])
+target = raw_df.values[1::2, 2]
+print("Data shape: ", data.shape)
+
+X, y = mglearn.datasets.load_extended_boston()
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+lr = LinearRegression().fit(X_train, y_train)
+
+print("Training set score: {:.2f}".format(lr.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(lr.score(X_test, y_test)))
